e1e158bd928d512fd52814672b0a25f271cb6407
Erica Onyegwu <ericaalbert44@gmail.com>
2022-07-14 17:29:07 -0700

    Magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,7 +14,6 @@ p = &n;
 * - you are not allowed to use a
 * - you are not allowed to modify p
 * - only one statement
-
 * - you are not allowed to code anything else than this line of code
 */
 *(p + 5) = 98;
