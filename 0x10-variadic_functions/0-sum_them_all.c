19cac05c11ce575f6240629bc72ec4bba72553f0
Erica Onyegwu <ericaalbert44@gmail.com>
2022-08-04 07:06:30 -0700

    0-main, Variadic functions, Sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -23,17 +23,3 @@ va_end(nums);
 
 return (sum);
 }
-
-Task 1 main.c
-#include "variadic_functions.h"
-
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-print_numbers(", ", 4, 0, 98, -1024, 402);
-return (0);
-}
